e0c9ab1c098cc4258e556afc8f82ccecf4644dbc
Masaki Sato <g5.taisa007@gmail.com>
2016-01-29 13:52:04 +0900

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+echo 'request' . PHP_EOL;
+var_dump($_REQUEST);
+
+echo 'session' . PHP_EOL;
+var_dump($_SESSION);
+
+echo 'server' . PHP_EOL;
+var_dump($_SERVER);
+
+echo 'cookie' . PHP_EOL;
+var_dump($_COOKIE);
+
+echo 'env' . PHP_EOL;
+var_dump($_ENV);
+
+echo 'file'. PHP_EOL;
+var_dump($_FILES);
+
+echo 'get' . PHP_EOL;
+var_dump($_GET);
+
+echo 'post' . PHP_EOL;
+var_dump($_POST);
